4ce1ac2e20564f5e3a730281eb08c91c72247e8c
Amir <78048332+asammoura@users.noreply.github.com>
2021-04-23 09:36:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,23 @@ text("Madlibs!",54,107);
     textSize(29);
     text("Instructions", 124, 287);
 };
-
+var instructionsScreen = function() {
+    background(94, 141, 191);
+    textSize(50);
+    text("Instructions",69,67);
+    textSize(20);
+    fill(255, 255, 255);
+    text("To begin the game, click 'Start!', you will then be prompted to choose 1 option at each directed step. After making 4 selections, click 'Generate' at the bottom of the screen to get your Madlibs paragraph!",20,107,300,400);
+    
+};
 splashScreen();
+mouseClicked = function() {
+    if (mouseX > 115 && mouseX < 276 &&
+    mouseY < 300 && mouseY > 255) {
+    instructionsScreen();
+    }
+};
+    
+draw = function() {
+    println(mouseX + ", " + mouseY);
+};
