d15df20e840893b7513a5b7f8b0659502d75fdd4
Amir <78048332+asammoura@users.noreply.github.com>
2021-05-02 17:23:26 -0400

    Update index.js
    added screens for each button and a return button from instructions page.
    
    needs to be done:
    if statements for each option in Start screen
    paragraph needs to be implemented with blank spaces filled in by "word[i]"

--- index.js
+++ index.js
@@ -40,11 +40,53 @@ var instructionsScreen = function() {
     fill(0, 0, 0);
     text("Got it!",160,330);
 };
+
+var nouns = ["Sock", "Baby", "Ship", "Chocolate"];
+var adjectives = ["Skinny", "Tall", "Strong", "Fickle"];
+var verbings = ["Running", "Falling", "Wrecking", "Calling"];
+var verbs = ["Sleep", "Swing", "Crawl", "Sling"];
+
+var startScreen = function() {
+    screen = 3;
+    background(94, 141, 191);
+    textSize(30);
+    fill(255, 255, 255);
+    text("Choose your Words",74,30);
+    textSize(18);
+    fill(255, 255, 255);
+    text("Noun:",15,70);
+    for (var i = 0; i < 10; i++) {
+        fill(97, 255, 184);
+        text(nouns[i], 15 + (i*85), 100);
+    }
+    fill(255, 255, 255);
+    text("Adjective:",15,145);
+    for (var i = 0; i < 10; i++) {
+        fill(97, 255, 184);
+        text(adjectives[i], 15 + (i*90), 175);
+    }
+    fill(255, 255, 255);
+    text("Verb ending in -ing:",15,220);
+    for (var i = 0; i < 10; i++) {
+        fill(97, 255, 184);
+        text(verbings[i], 15 + (i*90), 255);
+    }
+    fill(255, 255, 255);
+    text("Verb:",15,295);
+    for (var i = 0; i < 10; i++) {
+        fill(97, 255, 184);
+        text(verbs[i], 15 + (i*90), 330);
+    }
+};
 splashScreen();
 mouseClicked = function() {
     if (screen === 1 && mouseX > 115 && mouseX < 276 &&
     mouseY < 300 && mouseY > 255) {
     instructionsScreen();
+    }
+    if (screen === 1 && mouseX > 115 && mouseX < 276 &&
+    mouseY < 210 && mouseY > 165) {
+    startScreen();
     }
     if (screen === 2 && mouseX > 125 && mouseX < 276 &&
     mouseY < 340 && mouseY > 300) {
@@ -62,3 +104,4 @@ draw = function() {
     println(mouseX + ", " + mouseY);
 };
 */
+
